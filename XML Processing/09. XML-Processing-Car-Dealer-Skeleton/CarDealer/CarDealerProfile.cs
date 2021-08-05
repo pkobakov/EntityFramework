913e42f0962daf84b8552ed24bee1745a65ff1ab
Petar Kobakov <pkobakov@gmail.com>
2021-08-05 18:53:40 +0300

    XML Processing

---
+++ XML Processing/09. XML-Processing-Car-Dealer-Skeleton/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CarDealer.DataTransferObjects.InputModels;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            this.CreateMap<CustomerInputModel,Customer>();
+            this.CreateMap<SaleInputModel, Sale>();
+        }
+    }
+}
---
+++ XML Processing/09. XML-Processing-Car-Dealer-Skeleton/CarDealer/DataTransferObjects/OutputModels/SupplierOutputModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects.OutputModels
+{
+    [XmlType("suplier")]
+    public class SupplierOutputModel
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("parts-cout")]
+        public  int PartsCout { get; set; }
+    }
+}
