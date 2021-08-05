5c83861f028223bf52cca330cf8b2b45a1950581
Petar Kobakov <pkobakov@gmail.com>
2021-08-05 19:16:46 +0300

    JSON Processing

---
+++ JSON Processing/Product Shop/ProductShop/DataTransferObjects/CategoriesInputDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.DataTransferObjects
+{
+    public class CategoriesInputDto
+    {
+        public string Name  { get; set; }
+    }
+}
---
+++ JSON Processing/Product Shop/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ProductShop.DataTransferObjects;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            this.CreateMap<UserInputDto, User>();
+            this.CreateMap<ProductInputDto, Product>();
+            this.CreateMap<CategoriesInputDto, Category>();
+            this.CreateMap<CategoriesProductsDto, CategoryProduct>();
+        }
+    }
+}
