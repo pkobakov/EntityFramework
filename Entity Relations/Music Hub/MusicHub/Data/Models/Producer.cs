c3378b17d01e66fe5772d6f4ca950e5daa99db82
Petar Kobakov <pkobakov@gmail.com>
2021-12-29 17:53:48 +0200

    Entity Relations

---
+++ Entity Relations/Music Hub/MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Producer
+    {
+        public Producer()
+        {
+            Albums = new HashSet<Album>();
+        }
+        public int Id  { get; set; }
+        public string Name  { get; set; }
+        public string Pseudonym { get; set; }
+        public string PhoneNumber { get; set; }
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
