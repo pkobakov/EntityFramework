bd15852026732f2f51461e2c760ec58bf22ca8f6
Petar Kobakov <pkobakov@gmail.com>
2022-01-28 00:42:40 +0200

    SQL Injection Demo

---
+++ ADO.NET/CSharpDbDemo/SQL Injection Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+
+Console.Write("Please enter username: ");
+var username = Console.ReadLine();
+
+Console.Write("Please enter password: ");
+var password = Console.ReadLine();
+
+
+var connectionStringService = "Server= .; Database= Service;TrustServerCertificate=True; Integrated Security = true;";
+
+
+using (var connection = new SqlConnection(connectionStringService))
+{
+
+    connection.Open();
+
+    var command = new SqlCommand($"SELECT COUNT(*) FROM Users WHERE Username = '@Username' AND Password = '@Password'", connection);
+    command.Parameters.Add(new SqlParameter("@Username", username));
+    command.Parameters.Add(new SqlParameter("@Password", password));
+    var usersCount = (int)command.ExecuteScalar();
+    if (usersCount>0)
+    {
+        Console.WriteLine("Access granted! Welcome!");
+    }
+
+    else
+    {
+        Console.WriteLine("Access denied!");
+    }
+
+
+};
+
