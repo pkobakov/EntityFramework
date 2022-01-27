e65fe86c6c3eed3b298cd06b87a48375bfd1f8e6
Petar Kobakov <pkobakov@gmail.com>
2022-01-28 00:43:44 +0200

    SqlClient Demo

---
+++ ADO.NET/CSharpDbDemo/CSharpDbDemo.Library/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpDbDemo.Library
+{
+    public class Person
+    {
+        public Person(string name, string family, int age, string email)
+        {
+            Name = name;
+            Family = family;
+            Age = age;
+            Email = email;
+        }
+
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Family: {Family}, Age: {Age}, Email: {Email}";
+        }
+    }
+}
---
+++ ADO.NET/CSharpDbDemo/CSharpDbDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using CSharpDbDemo.Library;
+using Microsoft.Data.SqlClient;
+
+//-------------------------------------------------------------------------------------------------------
+
+  //Using Library
+
+var person = new Person("Ivan", "Ivanov", 55, "ivanov@gmail.com");
+
+//Console.WriteLine(person);
+
+
+
+var connectionStringSoftUni = "Server=.; Database= SoftUni;TrustServerCertificate=True; Integrated Security= true;";
+using (var firstConnection = new SqlConnection(connectionStringSoftUni))
+{
+    firstConnection.Open();
+
+    //Execute NonQuery - manipulates th database
+
+    var firstCommand = new SqlCommand("UPDATE Employees SET Salary = Salary + 0.12", firstConnection);
+    var rowsAffected = firstCommand.ExecuteNonQuery();
+    Console.WriteLine(rowsAffected);
+
+//------------------------------------------------------------------------------------------------------------------
+    //Execute Scalar - get data from the database and shows the first row-col that matches to the criteria
+
+    var secondCommand = new SqlCommand("SELECT FirstName FROM Employees WHERE FirstName LIKE 'G%'",connection);
+    var result = firstCommand.ExecuteScalar();
+    Console.WriteLine(result);
+
+//------------------------------------------------------------------------------------------------------------------
+    //Execute ExecuteReader - get data from database
+
+    var thirdCommand = new SqlCommand("SELECT * FROM Employees WHERE FirstName LIKE 'G%'", firstConnection);
+    var sqlReader = firstCommand.ExecuteReader();
+    while (sqlReader.Read())
+    {
+        Console.WriteLine(sqlReader["FirstName"]);
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
